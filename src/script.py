30c7c50bb140bade5cf3c75b7eda469c1c61afe0
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2023-11-09 20:31:12 -0300

    create script of data manipulation

---
+++ src/script.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from utils.text_manipulation import TextManipulation
+
+file_response = './form_responses/responses.xlsx'
+
+# df = pd.read_excel(file_response);
+
+# print( df['Descreva como você está se sentindo agora'] )
